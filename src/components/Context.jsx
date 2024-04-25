60c666844146addb968a40a24f84db166add93b6
Hanhtunaung97 <littlehanhtunaung97>
2024-04-25 21:02:55 +0630

    json server added

--- src/components/Context.jsx
+++ src/components/Context.jsx
@@ -1,24 +1,47 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Context = () => {
-    const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
+  console.log("update",count);
 
   const increment = () => {
     setCount(count + 1);
   };
 
   const decrement = () => {
-    setCount(count - 1);
+    count > 0 && setCount(count - 1);
   };
+  useEffect(() => {
+    console.log("component mount");
+    // fetch('https://fakestoreapi.com/products/1')
+    //         .then(res=>res.json())
+    //         .then(json=>console.log(json))
+    fetch('http://localhost:5173/api/courses')
+    .then(res=>res.json())
+    .then(json=>console.log(json))
+    return () => {
+      console.log("component unmount");
+    };
+  }, [count]);
   return (
     <div>
       <div className="p-4 bg-white rounded-md shadow-md mb-3">
         <p>This is the component to be hidden/shown.</p>
       </div>
       <div className="flex items-center justify-center space-x-4">
-        <button className="px-4 py-2 bg-blue-500 text-white rounded-md" onClick={decrement}>-</button>
+        <button
+          className="px-4 py-2 bg-blue-500 text-white rounded-md"
+          onClick={decrement}
+        >
+          -
+        </button>
         <span className="text-xl font-semibold">{count}</span>
-        <button className="px-4 py-2 bg-blue-500 text-white rounded-md" onClick={increment}>+</button>
+        <button
+          className="px-4 py-2 bg-blue-500 text-white rounded-md"
+          onClick={increment}
+        >
+          +
+        </button>
       </div>
     </div>
   );
